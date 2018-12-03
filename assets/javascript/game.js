92f9df55f2f8ec6f8ab85f380869b083ae87880c
Patrycja Gorczynska <patrycjagorczynska@ya-lr03v0bl.musd.mpsaz.org>
2018-12-03 14:53:17 -0700

    Finished Java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,103 @@
-
-
-
+var winningNumber = Math.floor(Math.random() * 150) + 60;
+var catScore = 0;
+var firstClick = Math.floor(Math.random () * 40) + 18;
+var secondClick = Math.floor(Math.random () * 10) + 2;
+var thirdClick = Math.floor(Math.random () * 30) + 10;
+var fourthClick = Math.floor(Math.random () * 20) + 1;
 var wins = 0;
 var losses = 0;
 
-var targetNumber = Math.floor(Math.random() * 150) + 60;
-var number = 0;
-var gemOne = Math.floor(Math.random () * 40) + 18;
-var gemTwo = Math.floor(Math.random () * 10) + 2;
-var gemThree = Math.floor(Math.random () * 30) + 10;
-var gemFour = Math.floor(Math.random () * 20) + 1;
+console.log(winningNumber);
 
-console.log(targetNumber);
+$(document).ready(function () {
 
-function play() {
-
-    $(".winning-number").text(targetNumber);
-     targetNumber = Math.floor(Math.random() * 150) + 60;
-    gemOne = Math.floor(Math.random () * 40) + 18;
-    gemTwo = Math.floor(Math.random () * 10) + 2;
-     gemThree = Math.floor(Math.random () * 30) + 10;
-    gemFour = Math.floor(Math.random () * 20) + 1;
-    number = 0;
-    $(".total-number").html(number);
-    $(".winning-number").html(targetNumber);
-    
+function gameReset () {
+    $("#winning-number").html(winningNumber);
+    winningNumber = Math.floor(Math.random() * 150) + 60;
+    catScore = 0;
+    firstClick = Math.floor(Math.random () * 40) + 18;
+    secondClick = Math.floor(Math.random () * 10) + 2;
+    thirdClick = Math.floor(Math.random () * 30) + 10;
+    fourthClick = Math.floor(Math.random () * 20) + 1;
 
 }
 
+$("#winning-number").html(winningNumber);
+$("#Wins").html(wins);
+$("#Losses").html(losses);
+
+$("#firstClick").click(function (event) {
+    catScore = catScore + firstClick;
+    $(firstClick).text(catScore);
+    $("#total-number").html(catScore);
+    if (catScore === winningNumber) {
+        alert("You won! Meow");
+        wins++
+        $("#Wins").html(wins);
+        gameReset();
+    } else if (catScore > winningNumber) {
+        alert("Hisssss, you lost");
+        losses++
+        $("#Losses").html(losses);
+        gameReset();
+    }
+});
+
+$("#secondClick").click(function (event){
+    catScore = catScore + secondClick;
+    $(secondClick).text(catScore);
+    $("#total-number").html(catScore);
+    if (catScore === winningNumber) {
+        alert("You won! Meow");
+        wins++
+        $("#Wins").html(wins);
+        gameReset();
+    } else if (catScore > winningNumber) {
+        alert("Hisssss, you lost");
+        losses++
+        $("#Losses").html(losses);
+        gameReset();
+    }
 
 
-    
+});
+
+$("#thirdClick").click(function (event) {
+    catScore = catScore + thirdClick;
+    $(thirdClick).text(catScore);
+    $("#total-number").html(catScore);
+    if (catScore === winningNumber) {
+        alert("You won! Meow");
+        wins++
+        $("#Wins").html(wins);
+     } else if (catScore > winningNumber) {
+        alert("Hissss, you lost");
+        losses++
+        $("#Losses").html(losses);
+        gameReset();
+     }
+
+});
+
+$("#fourthClick").click(function (event) {
+    catScore = catScore + fourthClick;
+    $(fourthClick).text(catScore);
+    $("#total-number").html(catScore);
+    if (catScore === winningNumber) {
+        alert("You won! Meow");
+        wins++
+        $("#Wins").html(wins);
+    } else if (catScore > winningNumber) {
+        alert("Hissss, you lost");
+        losses++
+        $("#Losses").html(losses);
+        gameReset(); 
+    }
+
+})
+
+
+
+});
+
+
